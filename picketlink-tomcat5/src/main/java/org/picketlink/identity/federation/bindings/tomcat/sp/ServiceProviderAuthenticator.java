e54cbb95df4a74137e67e45263ebc9cb8e9b478f
Marek Paterczyk <mpatercz@redhat.com>
2015-05-22 16:33:41 -0400

    Skip saml when x-skip-picketlink header is set

--- picketlink-tomcat5/src/main/java/org/picketlink/identity/federation/bindings/tomcat/sp/ServiceProviderAuthenticator.java
+++ picketlink-tomcat5/src/main/java/org/picketlink/identity/federation/bindings/tomcat/sp/ServiceProviderAuthenticator.java
@@ -1,6 +1,12 @@
 package org.picketlink.identity.federation.bindings.tomcat.sp;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
 
 
 /**
@@ -23,5 +29,17 @@ public class ServiceProviderAuthenticator extends AbstractSPFormAuthenticator {
     @Override
     protected String getContextPath() { 
         return getContext().getServletContext().getContextPath();
+    }
+
+    @Override
+    public void invoke(Request arg0, Response arg1) throws IOException,
+            ServletException {
+
+        if (arg0.getHeader("x-skip-picketlink") == null) {
+            logger.debug("Found x-skip-picketlink header, skipping saml authentication");
+            getNext().invoke(arg0, arg1);
+        } else {
+            super.invoke(arg0, arg1);
+        }
     }
 }
